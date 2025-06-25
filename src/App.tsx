0b2c11e65bc6ff7c06d0ddb2ee9ee9e48605a090
Profesional digital <profesionaldigital@impormedical.com.co>
2025-06-25 16:48:18 -0500

    RefContext uppercase

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Footer from './modules/components/footer/Footer';
 import HeroSection from './modules/components/hero-section/HeroSection';
 import NavComponent from './modules/components/nav-component/NavComponent';
 import OurServices from './modules/components/our-services/OurServices';
-import { RefContext } from './modules/shared/context/refContext';
+import { RefContext } from './modules/shared/context/RefContext';
 
 function App() {
 
